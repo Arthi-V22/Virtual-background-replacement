86b69e144d8d06b110de126a1712d6fdfd9fd206
Arthi-V22 <arthisree2206@gmail.com>
2025-05-19 14:30:07 +0530

    Add files via upload

--- arthi.py
+++ arthi.py
@@ -1,18 +1,18 @@
 import cv2
 import numpy as np
-image = cv2.imread("moon.jpg")
-background = cv2.imread("background.jpg")
-background = cv2.resize(background, (image.shape[1], image.shape[0]))
-mask = np.zeros(image.shape[:2], np.uint8)
-bgdModel = np.zeros((1, 65), np.float64)
-fgdModel = np.zeros((1, 65), np.float64)
-rect = (50, 50, image.shape[1]-50, image.shape[0]-50)
-cv2.grabCut(image, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
-mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-foreground = image * mask2[:, :, np.newaxis]
-output_image = np.where(foreground == 0, background, foreground)
-cv2.imwrite("output_image.jpg", output_image)
-cv2.imshow('Original Frame', image)
-cv2.imshow("Virtual Background", output_image)
+image = cv2.imread('marker_image.jpg')  
+image = cv2.resize(image, (640, 480))
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+_, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
+    if len(approx) == 4 and cv2.isContourConvex(approx):
+        area = cv2.contourArea(approx)
+        if area > 1000:
+            cv2.drawContours(image, [approx], 0, (0, 255, 0), 3)
+            cv2.putText(image, "Marker Detected", tuple(approx[0][0]), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+cv2.imshow("Marker Detection", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
